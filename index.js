474f40db5438f5159a17f52312cb9efda8be0a2f
nhhasib <nafiul.hasan1997@gmail.com>
2023-05-21 04:03:25 +0600

    all-toys/:id data added

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Toy Heaven running')
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uld9vql.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      
+      const toysCollection = client.db('toyHeaven').collection('allToys');
+
+      app.get('/all-toys', async (req, res) => {
+          const cursor = toysCollection.find();
+          const result = await cursor.toArray();
+          res.send(result)
+      })
+
+      app.get('/all-toys/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await toysCollection.findOne(query);
+          res.send(result)
+      })
+
+  
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
     console.log(`Toy heaven listening port,${port}`)
